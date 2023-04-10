68e03b2f40f3a82915cf24eba25e01d2cf2f7892
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2023-04-10 10:20:37 -0300

    aplicado uso do polimorfismo

---
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,17 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    //polimorfismo 
+    public void inclui(Titulo titulo) {
+        tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+
+}
