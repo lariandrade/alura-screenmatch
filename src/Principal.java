ef20edbb36a683bc1f3546994d07ffc6331f1617
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2023-04-10 10:55:43 -0300

    aplicando interface

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,12 @@
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
 
 public class Principal {
     public static void main(String[] args) {
-       /* Filme meuFilme = new Filme();
+        Filme meuFilme = new Filme();
         meuFilme.setNome("Barbie");
         meuFilme.setAnoDeLancamento(2023);
 
@@ -14,14 +16,24 @@ public class Principal {
         meuFilme.avalia(5);
         meuFilme.avalia(9);
 
-        System.out.println("Media de avaliacoes do filme: " + meuFilme.pegaMedia());
-        System.out.println("Total de avaliacoes: " + meuFilme.getTotalDeAvaliacoes()); */
+        //System.out.println("Media de avaliacoes do filme: " + meuFilme.pegaMedia());
+        //System.out.println("Total de avaliacoes: " + meuFilme.getTotalDeAvaliacoes()); 
+
         Serie lost = new Serie();
         lost.setNome("Lost");
         lost.setAnoDeLancamento(2000);
-        lost.exibeFichaTecnica();
+        //lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        //filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
 
 
     }
